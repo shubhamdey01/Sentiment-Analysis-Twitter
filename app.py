321304bce48a913ab0f19af73a093984223cbcda
shubhamdey01 <shubhamdeyjnv@gmail.com>
2024-08-24 16:28:14 +0530

    improved accuracy & modified webapp

--- app.py
+++ app.py
@@ -1,44 +1,88 @@
-import numpy as np
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 import pickle
-import streamlit as st
 import re
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import streamlit as st
 import nltk
-nltk.download('stopwords')
 
 # loading trained model
-model = pickle.load(open('twitterModel.pkl', 'rb'))
-vectorizer = pickle.load(open('tweetVectorizer.pkl', 'rb'))
+vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+model = pickle.load(open('models/model.pkl', 'rb'))
 
-def stemVectorize(text):
-    engStopwords = stopwords.words('english')
-    porterStemmer = PorterStemmer()
-    newText = re.sub('[^a-zA-Z]', ' ', text)
-    newText = newText.lower()
-    newText = newText.split()
-    newText = [porterStemmer.stem(word) for word in newText if not word in engStopwords]
-    newText = ' '.join(newText)
+# Pre-Processing Tweets
+# creating stemmer
+stemmer = PorterStemmer()
 
-    newText = vectorizer.transform([newText])
+# defining regex patterns
+url = r'((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)'
+user = r'@[^\s]+'
+alpha = r'[^a-zA-Z]'
+seq = r'(.)\1\1+'
+seqReplace = r'\1\1'
 
-    return newText
+# Cache the download of NLTK resources
+@st.cache_resource
+def downloadResources():
+    nltk.download('punkt_tab')
 
-def sentimentAnalysis(inputData):
-    prediction = model.predict(inputData)
-    return prediction[0]
-    
+def preprocessData(text):
+    # lowercasing
+    text = text.lower()
+
+    # removing URL's
+    text = re.sub(url, ' ', text)
+
+    # removing mentions @USER
+    text = re.sub(user, ' ', text)
+
+    # remove all non alphabets
+    text = re.sub(alpha, ' ', text)
+
+    # replace more than 2 consecutive letters by 2 letters
+    text = re.sub(seq, seqReplace, text)
+
+    # remove extra whitespaces
+    text = text.strip()
+    text = re.sub(r' +', ' ', text)
+
+    tokens = []
+
+    for word in word_tokenize(text):
+        # if word not in stopwordEng:
+        tokens.append(stemmer.stem(word))   # stemming
+
+    return ' '.join(tokens)
+
+
 def main():
-    st.set_page_config(page_title="Sentiment Analysis")
-    st.title('Sentiment Analysis : Twitter')
-    tweet = st.text_input('Tweet to analyze')
-    tweet = stemVectorize(tweet)
+    # page configuration
+    st.set_page_config(
+        page_title = 'Sentiment Analysis',
+        page_icon = 'images/pageLogo.jpeg',
+        layout = 'centered'
+    )
+
+    # page title
+    cols = st.columns([7, 1])
+    with cols[0]:
+        st.title('Sentiment Analysis of Tweets')
+    with cols[1]:
+        st.image('images/twitterSentiment.jpg', width=100)
+
+    # input
+    tweet = st.text_area('Tweet to analyze', height=200)
+
+    # making prediction
+    tweet = preprocessData(tweet)
+    tweet = vectorizer.transform([tweet])
+
     if st.button('Analyze'):
-        flag = sentimentAnalysis(tweet)
+        flag = model.predict(tweet)[0]
         if flag:
             st.success('Positive Tweet')
         else:
-            st.success('Negative Tweet')
+            st.error('Negative Tweet')
+
 
 if __name__ == '__main__':
     main()
